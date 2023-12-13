bf0ea08e24f64b217b280792d90de3ae18436ea6
ankitkarn2001 <153712258+ankitkarn2001@users.noreply.github.com>
2023-12-13 12:49:09 +0530

    Add files via upload

--- routes/diseaseRoute.js
+++ routes/diseaseRoute.js
@@ -1,1 +1,15 @@
+const express = require("express");
+const diseaseController = require("../controllers/diseaseController");
 
+const router = express.Router();
+
+router.route("/").get(diseaseController.allDisease);
+router.route("/addDisease").post(diseaseController.addDisease);
+router.route("/addDiscription/:id").put(diseaseController.updateDisease);
+
+router.route("/getAllSubTypeDisease").post(diseaseController.allSubTypeDisease)
+router.route("/getDiseaseDetail").post(diseaseController.getDiseaseDetail);
+router.route("/getDiseaseFromSymtom").post(diseaseController.getDiseaseFromSymtom)
+router.route("/getDiseaseFromSubTypeDisease").post(diseaseController.getDiseaseFromSubTypeDisease)
+
+module.exports = router;
