ec6f2b5f2f783f64e94a02d16a7292297cbed1ef
ankitkarn2001 <153712258+ankitkarn2001@users.noreply.github.com>
2023-12-13 12:52:44 +0530

    Add files via upload

--- models/diseaseModel.js
+++ models/diseaseModel.js
@@ -1,1 +1,55 @@
+const mongoose = require('mongoose');
+const slugify = require('slugify');
 
+
+const diseaseSchema = new mongoose.Schema({
+    diseaseName:{
+        type:String,
+        required:true,
+        trim:true,
+    },
+    diseaseDiscription:{
+        type:String,
+    },
+    diseasetype:{
+        type:String,
+        required: true,
+    },
+    diseaseSubtype:{
+        type:String,
+    },
+    causativeAgent: {
+        type: String,
+        text: true,
+      },
+    slug: String,
+    transmission:{
+        type: String
+    },
+    incubationPeriod:{
+        type: String,
+    },
+    symptoms:[String],
+    diagnosis:{
+        type:String,
+        trim:true,
+    },
+    treatment:{
+        type:String,
+        trim:true,
+    },
+    prevention:{
+        type:String,
+        trim:true,
+    },
+})
+
+// DOCUMENT MIDDLEWARE: runs before .save() and .create()
+diseaseSchema.pre("save", function (next) {
+    this.slug = slugify(this.diseaseName, { lower: true });
+    next();
+});
+
+const Disease = mongoose.model("medicalApp", diseaseSchema);
+
+module.exports = Disease;
